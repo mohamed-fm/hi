a3e0c2dac1cfc27356be6a6b3da11ced479a703f
mohamed-fm <141369494+mohamed-fm@users.noreply.github.com>
2024-04-10 09:50:39 +0200

    Add files via upload

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,8 @@ module.exports = function(eleventyconfig){
     
     eleventyconfig.addPassthroughCopy('./src/style.css');
     eleventyconfig.addPassthroughCopy('./src/assets');
+    eleventyconfig.addPassthroughCopy('./src/admain');
+
     
     return {
         dir: {
